2da18dfeaec76759b2b84a26d5824ba5cfbbe5a6
gustavo <gustavo.maia@b2b.com.pt>
2020-05-03 14:27:43 +0100

    Classes DataBase e Grupo

---
+++ Data/DataBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalEquador.Data
+{
+    public abstract class DataBase
+    {
+
+        public string IdCriador { get; set; }
+
+        public DateTime DataCriacao { get; set; }
+
+    }
+}
---
+++ Data/Grupo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalEquador.Data
+{
+    public class Grupo : DataBase
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+
+        public string Descricao { get; set; }
+
+
+    }
+}
