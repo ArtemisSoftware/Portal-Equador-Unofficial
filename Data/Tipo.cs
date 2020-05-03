258cb4364791598214ca8a5338366e0616bb209a
gustavo <gustavo.maia@b2b.com.pt>
2020-05-03 14:35:52 +0100

    Classe Tipo

---
+++ Data/Tipo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalEquador.Data
+{
+    public class Tipo : DataBase
+    {
+
+        [ForeignKey("IdGrupo")]
+        public int IdGrupo { get; set; }
+
+
+        [Key]
+        public int Id { get; set; }
+
+
+        public string Descricao { get; set; }
+
+
+        public string Codigo { get; set; }
+
+
+        public bool Ativo { get; set; }
+
+    }
+}
