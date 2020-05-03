d1dff7c6baa0a7cb823f9f1938d31184ea590cb6
gustavo <gustavo.maia@b2b.com.pt>
2020-05-03 15:01:15 +0100

    GrupoViewModel

---
+++ Models/GrupoViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalEquador.Models
+{
+    public class GrupoViewModel
+    {
+
+        public class CriarGrupoViewModel
+        {
+
+            [Required]
+            public string Descricao { get; set; } 
+
+            public DateTime DataCriacao { get; set; }
+        }
+
+
+        public class DetalhesGrupoViewModel
+        {
+
+            public int Id { get; set; }
+
+            public string Descricao { get; set; }
+
+            public DateTime DataCriacao { get; set; }
+        }
+
+    }
+}
