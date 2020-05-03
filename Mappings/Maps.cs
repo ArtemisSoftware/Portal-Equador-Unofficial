f6c2c499490569d5f91a8d6272efb68a44b2326d
gustavo <gustavo.maia@b2b.com.pt>
2020-05-03 15:12:59 +0100

    Mapeamento

---
+++ Mappings/Maps.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using PortalEquador.Data;
+using PortalEquador.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalEquador.Mappings
+{
+    public class Maps : Profile
+    {
+
+        public Maps()
+        {
+            CreateMap<Grupo, GrupoViewModel>().ReverseMap();
+        }
+
+    }
+}
