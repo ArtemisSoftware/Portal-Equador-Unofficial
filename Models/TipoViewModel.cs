cca314d1c893d86faa1b902ef0a836fe8fe65b39
gustavo <gustavo.maia@b2b.com.pt>
2020-05-05 11:29:20 +0100

    TipoViewModel

---
+++ Models/TipoViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalEquador.Models
+{
+    public class TipoViewModel
+    {
+
+        [Required]
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+
+
+        [Display(Name = "Código")]
+        public string Codigo { get; set; }
+
+
+        [Display(Name = "Data de criação")]
+        public DateTime DataCriacao { get; set; }
+
+
+        public bool Ativo { get; set; }
+
+    }
+}
