04dc02e53a9aca43680a95b96da0d527f42dd26a
gustavo <gustavo.maia@b2b.com.pt>
2020-05-06 14:23:17 +0100

    Classe contrato

---
+++ Data/Contrato.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalEquador.Data
+{
+    public class Contrato : DataBase
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+
+        [ForeignKey("IdEmpregado")]
+        public Empregado Empregado { get; set; }
+
+        public string IdEmpregado { get; set; }
+
+
+
+        [ForeignKey("IdFuncao")]
+        public Tipo Funcao { get; set; }
+
+        public int IdFuncao { get; set; }
+
+
+
+        [ForeignKey("IdEmpresa")]
+        public Tipo Empresa { get; set; }
+        public int IdEmpresa { get; set; }
+
+
+        public DateTime DataInicio { get; set; }
+
+
+
+
+        public DateTime DataTermino { get; set; }
+
+        public int Motivo { get; set; }
+
+
+        //public int Status { get; set; }
+
+
+
+        public bool Ativo { get; set; }
+    }
+}
--- Data/Tipo.cs
+++ Data/Tipo.cs
@@ -10,12 +10,16 @@ namespace PortalEquador.Data
     public class Tipo : DataBase
     {
 
-        [ForeignKey("IdGrupo")]
-        public int IdGrupo { get; set; }
-
-
         [Key]
         public int Id { get; set; }
+
+
+        [ForeignKey("RequestingEmployeeId")]
+        public Employee RequestingEmployee { get; set; }
+
+        public string RequestingEmployeeId { get; set; }
+
+
 
 
         public string Descricao { get; set; }
