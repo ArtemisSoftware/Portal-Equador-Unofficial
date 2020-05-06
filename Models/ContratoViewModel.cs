04ab47b383e7bea7834882dc801527157f8435fd
gustavo <gustavo.maia@b2b.com.pt>
2020-05-06 15:16:21 +0100

    ContratoViewModel

---
+++ Models/ContratoViewModel.cs
@@ -0,0 +1,38 @@
+﻿using PortalEquador.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalEquador.Models
+{
+    public class ContratoViewModel
+    {
+
+
+        //public Empregado Empregado { get; set; }
+
+
+        [Display(Name = "Empregado")]
+        public string IdEmpregado { get; set; }
+
+
+        public TipoViewModel Funcao { get; set; }
+
+        [Display(Name = "Função")]
+        public int IdFuncao { get; set; }
+
+
+
+        public TipoViewModel Empresa { get; set; }
+
+        [Display(Name = "Empresa")]
+        public int IdEmpresa { get; set; }
+
+
+        [Display(Name = "Data Inicio")]
+        public DateTime DataInicio { get; set; }
+
+    }
+}
