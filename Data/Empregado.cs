1d5453fa4b7c9d118f6b8cbfbbd8ab11fdbb4df4
gustavo <gustavo.maia@b2b.com.pt>
2020-05-06 12:13:18 +0100

    Classe Empregado

---
+++ Data/Empregado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalEquador.Data
+{
+    public class Empregado /*: IdentityUser*/
+    {
+
+        public string NumeroMecanografico { get; set; }
+
+        public string Nome { get; set; }
+
+    }
+}
